cef913061291cc7c855d022e46a71d6b0fd456cc
shubhampal <shubhampalatwork@gmail.com>
2024-07-16 00:32:45 +0530

    updated ui for homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,27 +3,43 @@ import { Typography, Button, Box } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
 function Home() {
-  return (
-    <Box sx={{ textAlign: 'center', mt: 4 }}>
-      <Typography variant="h4" gutterBottom>
-        Welcome to Email Sender
-      </Typography>
-      <Typography variant="body1" paragraph>
-        Create templates, manage datasets, and send personalized emails.
-      </Typography>
-      <Box>
-        <Button variant="contained" component={RouterLink} to="/template" sx={{ m: 1 }}>
-          Create Template
-        </Button>
-        <Button variant="contained" component={RouterLink} to="/dataset" sx={{ m: 1 }}>
-          Manage Dataset
-        </Button>
-        <Button variant="contained" component={RouterLink} to="/send" sx={{ m: 1 }}>
-          Send Emails
-        </Button>
-      </Box>
-    </Box>
-  );
+    return (
+        <Box sx={{ textAlign: 'center', mt: 4 }}>
+            <Typography variant="h4" gutterBottom>
+                Welcome to Email Sender
+            </Typography>
+            <Typography variant="body1" paragraph>
+                Create templates, manage datasets, and send personalized emails.
+            </Typography>
+            <Box>
+                <Button variant="contained" component={RouterLink} to="/template" sx={{
+                    m: 1,
+                    '&:hover': {
+                        color: 'white'
+                    }
+                }}>
+                    Create Template
+                </Button>
+                <Button variant="contained" component={RouterLink} to="/dataset" sx={{
+                    m: 1,
+                    '&:hover': {
+                        color: 'white'
+                    }
+                }}>
+                    Manage Dataset
+                </Button>
+                <Button variant="contained" component={RouterLink} to="/send" sx={{
+                    m: 1,
+                    '&:hover': {
+                        color: 'white'
+                    }
+                }}
+                >
+                    Send Emails
+                </Button>
+            </Box>
+        </Box>
+    );
 }
 
 export default Home;
